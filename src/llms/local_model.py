c88cc195ef57c18c4f2a3747e938e4fc1dd14501
Jokerjay <578055568@qq.com>
2025-05-15 20:42:26 +0800

    refractor：取消前缀kv cache手动深拷贝

--- src/llms/local_model.py
+++ src/llms/local_model.py
@@ -8,7 +8,6 @@ from pydantic import Field
 from transformers.cache_utils import (
     DynamicCache,
 )
-import copy
 import logging
 logging.basicConfig(filename=log_path, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
@@ -79,8 +78,6 @@ class LocalLLM(CustomLLM):
         with torch.no_grad():
             outputs = self.model(**prefix_inputs, use_cache=True)
             self.prefix_cache = outputs.past_key_values
-            # self.prefix_cache = DynamicCache()
-            # self.prefix_cache = self.model(**prefix_inputs, past_key_values = self.prefix_cache).past_key_values
     
     @property
     def metadata(self):
@@ -120,21 +117,13 @@ class LocalLLM(CustomLLM):
             CompletionResponse: 生成的文本响应
         """
         try:
-            # docs = '''[ 2023-07-28 16：40 ] ，正文：人民网柏林7月28日电 （记者刘仲华、张慧中）7月27日晚，中国驻德国大使馆举行庆祝中国人民解放军建军96周年招待会。德军政官员、各界人士、多国驻德使节和武官、华侨华人、留学生及中资企业代表等约350人出席招待会。吴恳大使和使馆主要外交官出席活动。国防武官吴俊辉少将在招待会上致辞。吴俊辉强调，中国人民解放军建军96周年来，在中国共产党领导下，为民族独立和解放，为国家建设和发展，为维护国家主权、安全和发展利益，建立了不朽功勋。中国军队永远是捍卫国家主权和领土完整的坚定力量。吴俊辉针对当前涉中国问题的负面杂音，简要阐述了中国和平理念和国防政策，指出历史是最好的见证者，中国是全球唯一将“坚持和平发展道路”载入宪法的国家，在涉及和平与安全问题上，中国更是纪录最好的大国。中国从不追逐霸权，也绝不走“国强必霸”的老路，中国军队始终是维护世界和平的坚定力量。吴俊辉表示，李强总理上月对德国进行了正式访问，传承了两国友谊，深化了双边合作。今年初，中德两国国防部举行了工作对话，前不久，两国防长在香格里拉对话会期间进行了会晤，为两军关系下一步发展指明了方向。中国军队愿同德方一道，加强沟通交流，增进理解互信，深化务实合作，共同维护世界和地区安全与稳定。招待会气氛热烈友好，现场还通过电子屏幕滚动播放《我在》等视频。出席来宾纷纷祝贺近年来中国国防和军队建设取得的巨大成就，赞赏中国军队为维护世界和平所做出的积极贡献。分享让更多人看到'''
-            # docs = '''人民网柏林7月28日电 中国驻德国大使馆27日晚举办建军96周年招待会，德军政界、多国使节及各界代表约350人出席。国防武官吴俊辉少将强调，中国军队在中国共产党领导下为国家发展和安全作出重大贡献，是捍卫国家主权的坚定力量。他重申中国将"和平发展"写入宪法，坚持永不称霸，致力于维护世界和平。吴俊辉提及李强总理访德成果及中德国防部近期对话，表示愿深化两军交流合作。活动期间，来宾高度评价中国国防建设成就及维和贡献，现场播放相关主题视频。'''
-            # docs_inputs = self.tokenizer(docs, return_tensors="pt").to(self.model.device)
-            # with torch.no_grad():
-            #     docs_cache = DynamicCache()
-            #     docs_cache = self.model(**docs_inputs, past_key_values = docs_cache).past_key_values
-            # past_key_values = self.stack_past_key_values([self.prefix_cache, docs_cache])
-            # past_key_values = self.stack_past_key_values([self.prefix_cache])
             # 在生成开始前就开始计时
             start_time = time.perf_counter()
             new_prompt = self.PREFIX+prompt+'<|im_end|><|im_start|>assistant\n'
             first_token_received = False
             inputs = self.tokenizer(new_prompt, return_tensors="pt").to(self.model.device)
             if self.use_kv_cache:
-                past_key_values = copy.deepcopy(self.prefix_cache)
+                past_key_values = self.prefix_cache
                 if key_values is not None:
                     past_key_values = self.stack_past_key_values([past_key_values] + key_values)
 
@@ -182,7 +171,7 @@ class LocalLLM(CustomLLM):
             new_prompt = self.PREFIX+prompt+'<|im_end|><|im_start|>assistant\n'
             inputs = self.tokenizer(new_prompt, return_tensors="pt").to(self.model.device)
             if self.use_kv_cache:
-                past_key_values = copy.deepcopy(self.prefix_cache)
+                past_key_values = self.prefix_cache
                 if key_values is not None:
                     stack_time = time.perf_counter()
                     past_key_values = self.stack_past_key_values([past_key_values] + key_values)
